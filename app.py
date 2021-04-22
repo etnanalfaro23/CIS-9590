acc5cc6add200366b14591f225aa601cb71c6b32
etnanalfaro23 <82787918+etnanalfaro23@users.noreply.github.com>
2021-04-22 13:15:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/etnan', methods=['GET', 'POST'])
+def etnan():
+    val = request.form['searchval']
+    return render_template('new.html',val=val)
 if __name__ == '__main__':
     app.run(debug=True)
