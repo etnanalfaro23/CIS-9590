ca2b4d18b95c0d6a494f6665a2eca08c31598d39
etnanalfaro23 <82787918+etnanalfaro23@users.noreply.github.com>
2021-05-15 11:39:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request
 from getdata import getTweets, getFinanaceInfo
 from helpers import loadmodels
 from processdata import processSentiment, processwordcloud
-
+import pandas as pd
+from flask_wtf import FlaskForm
+from wtforms import SelectField, validators
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR SECRET KEY'
 
-@app.route("/")
+@app.route("/" , methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    form = ReusableForm(request.form)
+    return render_template('index.html', form=form)
 
 @app.route('/etnan', methods=['GET', 'POST'])
 def etnan():
@@ -18,8 +22,8 @@ def etnan():
     financedata, graphdata, timestamps = getFinanaceInfo(val)
     pos_percentage, neg_percentage = processSentiment(data, countvect, model)
     wordfrequency = processwordcloud(data)
-    return render_template('Info_page.html', financedata = financedata, 
-                    pos_percentage = pos_percentage, 
+    return render_template('Info_page.html', financedata = financedata,
+                    pos_percentage = pos_percentage,
                     neg_percentage = neg_percentage,
                     val = val,
                     timestamps = timestamps,
@@ -27,6 +31,18 @@ def etnan():
                     wordfrequency = wordfrequency
                      )
 
+df = pd.read_excel("dataset.xlsx")
+
+def getCompanies():
+    stockticker = df["Stock Ticker"]
+    company = df["Company"]
+    results = dict(zip(stockticker, company))
+    return results
+
+class ReusableForm(FlaskForm):
+    companies = getCompanies()
+    name = SelectField("Enter a Ticker",
+                       choices=[(ticker, name) for ticker, name in companies.items()], validators=[validators.InputRequired()])
 
 if __name__ == '__main__':
     app.run(debug=True)
