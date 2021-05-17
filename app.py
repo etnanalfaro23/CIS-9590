aa26f0ae38cdb60a7afd2a1686703ba8394af08a
iaugustine <iaugustine9401@gmail.com>
2021-05-17 11:32:26 -0400

    changed css ratio and added NEWS links

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-from getdata import getTweets, getFinanaceInfo
+from getdata import getTweets, getFinanaceInfo, getNews
 from helpers import loadmodels
 from processdata import processSentiment, processwordcloud
 import pandas as pd
 from flask_wtf import FlaskForm
-from wtforms import SelectField, validators
+from wtforms import SelectField, validators, RadioField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR SECRET KEY'
@@ -17,18 +17,25 @@ def home():
 @app.route('/etnan', methods=['GET', 'POST'])
 def etnan():
     val = request.form['searchval']
-    data = getTweets(val)
+    stock_name = df.loc[df["Stock Ticker"]==val, 'Company'].item().strip()
+
+    print(stock_name)
+    data = getTweets(val, stock_name)
+    print(len(data))
     countvect, model = loadmodels()
+    headlines = getNews(val, stock_name)
+    print(headlines)
     financedata, graphdata, timestamps = getFinanaceInfo(val)
     pos_percentage, neg_percentage = processSentiment(data, countvect, model)
-    wordfrequency = processwordcloud(data)
+    wordfrequency = processwordcloud(data,val)
     return render_template('Info_page.html', financedata = financedata,
                     pos_percentage = pos_percentage,
                     neg_percentage = neg_percentage,
                     val = val,
                     timestamps = timestamps,
                     data = graphdata,
-                    wordfrequency = wordfrequency
+                    wordfrequency = wordfrequency,
+                    headlines = headlines
                      )
 
 df = pd.read_excel("dataset.xlsx")
@@ -37,6 +44,7 @@ def getCompanies():
     stockticker = df["Stock Ticker"]
     company = df["Company"]
     results = dict(zip(stockticker, company))
+
     return results
 
 class ReusableForm(FlaskForm):
--- processdata.py
+++ processdata.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+
 def processSentiment(clean_text, count_vect, model):
     count = len(clean_text)
     pos = 0
@@ -14,12 +17,12 @@ def processSentiment(clean_text, count_vect, model):
             neg += 1
     pos_percentage = (pos/count) * 100
     neg_percentage = (neg/count) * 100
-    return pos_percentage, neg_percentage
+    return format(pos_percentage, '.2f'), format(neg_percentage, '.2f')
 
 def processfinancialCharts(data):
     pass
 
-def processwordcloud(textdata):
+def processwordcloud(textdata,val):
     # Convert all lists into one string
     str1 = " " 
     test = str1.join(textdata)
@@ -27,13 +30,26 @@ def processwordcloud(textdata):
     # Break up all words into unique characters
     str_list = test.split()
     unique_words = set(str_list)
+    
+
+    unique_words2 = [t for t in unique_words if t not in ['atuser','url'] and t not in [val]]
+    unique_words2 = [t for t in unique_words2 if t not in STOPWORDS]
+    print('from wordcloud', val, len(unique_words2))
 
     # Get word counts for each word
     word = []
-    for unique in unique_words:
+    for unique in unique_words2:
         word.append([unique, str_list.count(unique)])
 
-    jsonlist = pd.DataFrame(word, columns=('x','value')).to_json(orient="records")
+    
+    sorted = pd.DataFrame(word, columns=('x','value')).sort_values(by=['value'], ascending=False)
+    length = int(len(sorted)*0.8)
+    if length<500:
+        jsonlist = sorted[:length].to_json(orient="records")
+    else:
+        jsonlist = sorted.to_json(orient="records")
+
+    
 
     return jsonlist
 
